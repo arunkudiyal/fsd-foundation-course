2eebcb7ee38cb14e0cb4d2dd424ff22f6ebb04d4
arunkudiyal <arunkudiyal@icloud.com>
2022-01-07 19:46:56 +0530

    Day 05

---
+++ JavaScript/dom.js
@@ -0,0 +1,223 @@
+// Explore the document
+// console.dir(document)       // Object
+
+// Access the content from the document
+// console.log(document.URL);
+// console.log(document.title);
+// console.log(document.domain);
+
+// Accessing the HTML Element in the docuumnet
+// const title = document.all[10]
+// const secondTitle = document.all[13]
+
+// console.log(secondTitle);
+// secondTitle.textContent = `Don't Add Items!`
+// title.textContent = `Hello, I am a New Title`
+
+
+
+// IDENTIFIER METHODS - id, className, tagName, querySelector, querySelectorAll
+
+// 1. id
+// const heading = document.getElementById('header-title')
+
+// Changes you can perform
+// heading.textContent = `New Text`
+// heading.innerHTML = `<h3>New Text</h3>`
+
+// CSS Changes
+// background-color ---> camelCase ---> backgroundColor
+// heading.style.borderBottom = `5px solid black`
+
+
+// 2. className
+// const items = document.getElementsByClassName('list-group-item')
+// for(let i=0; i < items.length; i++) {
+//     if(i % 2 == 0) {
+//         items[i].style.backgroundColor = 'grey'
+//     }
+// }
+
+
+// 3. tagName
+// const items = document.getElementsByTagName('li')
+// for(let i=0; i < items.length; i++) {
+//     if(i % 2 == 0) {
+//         items[i].style.backgroundColor = 'grey'
+//     }
+// }
+
+
+
+// 4. querySelector - id + class + tag
+// const head =  document.querySelector('#header-title')
+// console.log(head);
+
+// const items = document.querySelector('.list-group-item')
+// console.log(items);
+
+// const itemsTwo = document.querySelector('li')
+// console.log(itemsTwo);
+
+
+// 5. querySelectorAll - id + class + tag
+// const head =  document.querySelectorAll('#header-title')
+// console.log(head);
+
+// const items = document.querySelectorAll('.list-group-item')
+// console.log(items);
+
+// const itemsTwo = document.querySelectorAll('li')
+// console.log(itemsTwo);
+
+
+
+// DOM EVENTS - addEventListner
+
+// function vaishnaviEvent() {
+//     alert('Button was Clicked!')
+// }
+
+// const button = document.querySelector('#button')
+// // element.addEventListener(nameOfTheEvent, nameOfTheFunctionality)
+// button.addEventListener('dblclick', vaishnaviEvent)
+
+
+// INPUT_TEXT EVENTS
+// const text = document.querySelector('#text-input')
+
+// text.addEventListener('focus', () => {
+//     console.log(`FOCUSED ON ME`);
+// })
+
+// text.addEventListener('blur', () => {
+//     console.log(`BLURRED ON ME`);
+// })
+
+// text.addEventListener('cut', () => {
+//     console.log(`CUT EVENT`);
+// })
+
+// text.addEventListener('copy', () => {
+//     console.log(`COPY EVENT`);
+// })
+
+// text.addEventListener('paste', () => {
+//     console.log(`PASTE EVENT`);
+// })
+
+
+// EVENT EMITTERS | Event Parameter
+// A parameter which is a parameter to a function which is listening to an event
+// const textInput = document.getElementById('text-input')
+// KeyUp & KeyDown
+// textInput.addEventListener('keyup', () => {
+//     console.log('Key has been relaesed');
+// })
+
+// textInput.addEventListener('keydown', () => {
+//     console.log('Key is being pressed');
+// })
+
+// Access the data that my user is trying to type
+// textInput.addEventListener('keyup', (event) => {
+//     console.log(event.target.value);
+// })
+
+// button = document.getElementById('submitBtn')
+// button.addEventListener('click', (e) => {
+//     // Prevents the default nature of the submit button to submit the form
+//     e.preventDefault();
+//     console.log(e);
+// })
+
+// FUN ACTIVITY WITH THE BOX
+// const box = document.getElementById('box')
+// box.addEventListener('mouseenter', (e) => {
+//     box.style.backgroundColor = `rgb(${e.offsetX}, ${e.offsetY}, 100)`
+// })
+
+// box.addEventListener('mouseleave', (e) => {
+//     box.style.backgroundColor = `rgb(${e.offsetX}, ${e.offsetY}, 150)`
+// })
+
+
+
+// DOM TRAVERSALS - A method to access HTML Elements based on relationships
+// parent, children, siblings
+
+// 1. PARENT NODES
+// const list = document.getElementById('items')
+// list.parentElement.style.backgroundColor = 'yellow'
+
+// chaining of DOM
+// console.log(list.parentElement.parentElement);
+// list.parentElement.parentElement.style.backgroundColor = 'grey';
+
+// FUN ACTIVITY
+// const button = document.getElementById('button')
+// button.addEventListener('click', () => {
+//     list.parentElement.style.backgroundColor = 'yellow';
+// })
+
+
+// CHILDREN NODES
+// console.log(list.children);
+// console.log(list.children[0]);
+
+// firstChild, lastName
+// console.log(list.firstChild);                // text -> Indentation
+// console.log(list.firstElementChild);
+
+// console.log(list.lastChild);                 // text -> Indentation
+// console.log(list.lastElementChild);
+
+
+
+// SIBLINGS
+// console.log(list.previousSibling);              // text -> Indentation
+// console.log(list.previousElementSibling);
+
+// console.log(list.nextSibling);
+// console.log(list.nextElementSibling);
+
+
+
+// VVVVVVV IMPORTANT
+// Creating HTML Elements
+
+// const newLI = document.createElement('li')
+
+// Set the Attributes of the HTML Element
+// newLI.id = 'new-li'
+// newLI.className = 'list-group-item'
+// newLI.textContent = `Item 7`
+
+// const list = document.getElementById('items')
+// // Add this in the ul list
+// list.appendChild(newLI)
+
+// console.log(list.lastElementChild);
+
+// console.log(newLI);
+
+
+
+// Adding Items through Form
+// const newLI = document.createElement('li')
+// newLI.className = 'list-group-item'
+
+// const text = document.getElementById('text-input')
+// text.addEventListener('keyup', (e) => {
+//     const textData = e.target.value
+//     newLI.textContent = textData
+//     console.log(newLI);
+//     const button = document.getElementById('submitBtn')
+//         button.addEventListener('click', (e) => {
+//         e.preventDefault()
+//         const list = document.getElementById('items')
+//         list.appendChild(newLI)
+//     })
+// })
+
+// console.log(newLI);
---
+++ JavaScript/scripts.js
@@ -0,0 +1,409 @@
+// JS is a programming language, embed logic in your apps
+
+
+// console.log('Hello...!')
+// console.log('Hello...') // Testing the app
+
+// Displaying outputs
+// alert('Alert Occured!')
+// console.warn('This is a warning')
+// console.error('This is an error')
+
+// JS BASICS
+// Variables & constants -> variableName = variableValue(constant)
+
+// DataTypes - Primitive DataType -  Number, String, Boolean, Null, Undefined & Symbols
+// Non-Primitive DataTypes - Array, Objects & ArrayOfObjects, JSON 
+
+a = 20
+// b = 'Innomatics Research Labs'
+// b = "Innomatics Research Labs"
+b = `Innomatics Research Labs`
+
+c = true
+d = undefined
+e = null
+
+// console.log(a, b, c, d, e);
+
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+// console.log(typeof d);
+// console.log(typeof e);  // null --> object ?????
+
+// DECLARATION OF A VARIABLE - let, var, const (ES6)
+
+// 1. var keyword is GLOBAL in nature - PUBLIC
+// 2. let(ES6) keyword is used for scoping - LOCAL
+// 3. const(ES6) keyword - LOCAL
+
+var anyValue = 'Hi, I am a Data'
+// console.log(anyValue)
+
+let anotherValue = 25
+let myLetValue
+// console.log('anotherValue ' + anotherValue);
+
+anotherValue = true
+// console.log('anotherValue ' + anotherValue);
+// console.log(anotherValue)
+
+const myName = 'Arun'
+// const myConstValue --> SHOULD NOT DO
+// myName = 100
+// myName = 'Some other Name' - DONT 
+
+// You cannot just declare a const value, you have to declare and define the value then & there itself
+// const constantValue
+// constantValue = 'New Value' // DONT
+
+
+// STRINGS & STRING METHODS
+
+// const str = 'This is a string'
+
+// Find the length of the String
+// console.log(str.length);
+
+// Chang the case
+// console.log(str.toUpperCase())
+// console.log(str.toLowerCase())
+
+// Sub-String
+// console.log(str.substr(7))
+// console.log(str.substr(13))         // startIndex (inc) till the end
+// console.log(str.substring(3, 12));  // startIndex(inc), endIndex(exc)
+
+// Split
+// const diffProgLanguages = str.split(', ');       // Return you with an 'Array'
+// console.log(diffProgLanguages);
+// console.log(diffProgLanguages[4]);
+
+// indexOf - returns the index value of alphabet, return -1 if the alphabet doesn't exist
+// console.log('Index of i : ' + str.indexOf('z'));
+
+// String Concatination (Merging the strings) & String Literals
+const myLuvkyNumber = 5
+// console.log('My lucky number is ' + myLuvkyNumber + ', this is an example');
+
+// Strings - '', "", `` - ${ variableName }
+// console.log(`My lucky number is ${myLuvkyNumber}, this is an example`);
+
+
+// ARRAYS IN JAVASCRIPT - Collection of Items
+// 2 ways Array - 1. Using Array Contrsuctor
+
+// const names = new Array('Dipti', 'Priya', 'Srija', 'Arun', 'Vamsi')
+// console.log(names)
+// // Access the data from the Array
+// console.log(names[3]);
+
+// ES6 - Use of [ ] symbol
+
+// const courses = ['Data Science', 'FSD', 'Machine Learning', 'Data Structures']
+// console.log(courses);
+// // Access 
+// console.log(courses[1]);
+
+// // Contain multiple data types in a single Array
+// const items = [10, 10.5, 'Arun', true, null, undefined]
+// console.log(items);
+// console.log(items[3]);
+
+
+// OBJECTS IN JS - { key: value }
+// const user = {
+//     firstName: 'Harry',
+//     lastName: 'Potter',
+//     age: 22,
+
+//     // include Arrays inside of an object
+//     hobbies: ['Magic', 'Getting in trouble', 'Wandering'],
+//     role: 'Student',
+//     isCool: true,
+
+//     // include objects within object
+//     address: {
+//         street: '5th Avenue',
+//         city: 'London',
+//         country: 'United Kingdoms'
+//     }
+// }
+
+// console.log(user);
+
+// Access the values from the Object
+// console.log(user.isCool);
+// console.log(user.hobbies[0])
+// console.log(user.address.city)
+// console.log(`Hi, my name is ${user.firstName} ${user.lastName}. I am ${user.age} years old. I am a ${user.role}. My hobbies are ${user.hobbies[0]}, ${user.hobbies[2]}. I live in ${user.address.city}`);
+
+
+// ARRAY OF OBJECTS ->  [ {}, {}, {}, {} ]
+
+// const employees = [
+//     {
+//         emp_id: 1001,
+//         designation: 'SDE',
+//         company: 'Wipro',
+//         yearsOfExp: 3,
+//         skiils: ['C', 'C++', 'Java'],
+//         location: {
+//             officeLoaction: 'Mumbai',
+//             pin: 12345
+//         }
+//     }, 
+//     {
+//         emp_id: 1023,
+//         designation: 'Tester',
+//         company: 'Facebook',
+//         yearsOfExp: 3,
+//         skiils: ['Mocha', 'JUnit'],
+//         location: {
+//             officeLoaction: 'Hyderabad',
+//             pin: 12342
+//         }
+//     }, 
+//     {
+//         emp_id: 1021,
+//         designation: 'SME',
+//         company: 'Apple',
+//         yearsOfExp: 5,
+//         skiils: ['JS', 'React', 'Angular'],
+//         location: {
+//             officeLoaction: 'Bangalore',
+//             pin: 12323
+//         }
+//     }, 
+//     {
+//         emp_id: 1065,
+//         designation: 'Content Designer',
+//         company: 'Netflix',
+//         yearsOfExp: 2,
+//         skiils: ['Wordpress', 'Photoshop'],
+//         location: {
+//             officeLoaction: 'Delhi',
+//             pin: 12001
+//         }
+//     }
+// ]
+
+// console.log(employees);
+
+// console.log(employees[0].company)
+// console.log(employees[0].skiils[1])
+
+// Accessing the ArrayOfObjects
+
+// 1. I want the emp_id of each employee
+// console.log(`${employees[0].emp_id}, ${employees[1].emp_id}, ${employees[2].emp_id}, ${employees[3].emp_id}`);
+
+// // 2. One skill from each employee
+// console.log(`${employees[0].skiils[0]}, ${employees[1].skiils[0]}, ${employees[2].skiils[0]}, ${employees[3].skiils[0]}`);
+
+// // 3. Office Loaction of all employees
+// console.log(`${employees[0].location.officeLoaction}, ${employees[1].location.officeLoaction}, ${employees[2].location.officeLoaction}, ${employees[3].location.officeLoaction}`);
+
+
+// CONDITIONALS - if, else
+// if(employees[0].yearsOfExp < 3) {
+//     console.log('Not fit for JOB');
+// } else {
+//     console.log('Fit for the JOB');
+// }
+
+// Short hand -> condition ? true : false
+// employees[1].yearsOfExp > 3 ? console.log('FIT FOR JOB') : console.log('NOT FIT FOR JOB'); 
+
+// if, else-if, else
+
+// AGE TRACKER - < 18 [KID], >=18 TEEN <30, >=30 ADULT
+// const age = 30
+// if(age < 18) {
+//     console.log('Hey, you are a KID')
+// } else if(age >= 18 && age < 30) {
+//     console.log('Hey, you are TEEN')
+// } else {
+//     console.log('Hey you are an ADULT')
+// }
+
+
+// *** FUN STUFF ***
+
+// const answer = 10 / 0
+// console.log(answer)             // Infinity
+
+// const answer1 = 20 / 'Arun'
+// console.log(answer1);           // NaN
+
+// const answer2 = 'Hi' / 'Arun'
+// console.log(answer2);           // NaN
+
+// const values = ['🥚', '🐓']
+// // Sort the values
+// const output = values.sort()
+// console.log(output);
+
+// if(11 === 11) {
+//     console.log('They are same');
+// } else {
+//     console.log('They are NOT same');
+// }
+
+
+// JSON - JS Object Notation
+// console.log(employees);
+
+// // stringify() - Convert ArrayOfObjects into JSON
+// const jsonData = JSON.stringify(employees)
+// console.log(jsonData);
+
+// // parse() - Convert JSON into ArrayOfObject
+// const arrayOfObjects = JSON.parse(jsonData)
+// console.log(arrayOfObjects);
+
+
+// var, let & const
+// age > 30 - half the age : double the age
+
+// const age = 30
+// if(age > 30) {
+//     const newAge = age / 2
+//     console.log(newAge)
+// } else {
+//     const newAge = age * 2
+//     console.log(newAge)
+// }
+
+// console.log(newAge);
+
+
+// LOOPS
+
+// 1. Basic Loops - for() & while()
+// 2. Higher Order Array Methods - for-of, for-in, map, filter, reduce
+
+// Print numbers from 1 to 15
+
+// for(initialization; condition; inc/dec)
+// for(let i=1; i <= 15; i++) {
+//     console.log(`Value is ${i}`);
+// } 
+
+// // Table of 5 
+// for(let i=5; i <= 50; i=i+5) {
+//     console.log(`5 x ${i/5} = ${i}`)
+// }
+
+// initilaization
+// while(condition) {
+//      ..body
+//     inc/dec
+// }
+
+// let i = 1
+// while(i <= 10) {
+//     console.log(`The value is ${i}`)
+//     i+=1
+// }
+
+// // Priniting the Table of 2
+// let j = 10
+// while(j <= 100) {
+//     console.log(`10 x ${j/10} = ${j}`);
+//     j+=10
+// }
+
+
+// Array of Object
+// const todos = [
+//     {
+//         id: 1,
+//         task: 'Meeting with the Team',
+//         isCompleted: true
+//     },
+//     {
+//         id: 2,
+//         task: 'Take out the Trash',
+//         isCompleted: false
+//     },
+//     {
+//         id: 3,
+//         task: 'Take my dog for a walk',
+//         isCompleted: true
+//     }
+// ]
+
+// const fruits = ['Apple', 'Mango', 'Banana', 'Pear', 'Watermelon']
+// console.log(fruits.length);
+
+// Length of the array
+// console.log(todos.length);
+
+// Agenda - 
+// for(let i=0; i < todos.length; i++) {
+//     console.log(todos[i].task);
+// }
+
+// 1. You are iterating on the whole array
+// 2. If you don't wanna use array[index]
+
+// for-of - for(let local of array) { ... }
+
+// for(let todo of todos) {
+//     // print only those tasks whose isCompleted is true
+//     todo.isCompleted === true ? console.log(todo.task) : ''
+// }
+
+
+// *** FUNCTIONS ***
+// Re-usable block of code that you can use anywhere in the code
+
+// 1. Traditional Way
+// 2. ES6 Way - New & Most Popular
+
+// 1. function functionName(parameters) { ...bodyOfFunction }
+
+// function product(a, b) {
+//     if(typeof a == 'number' && typeof b == 'number') {
+//         return a * b
+//     } else {
+//         return `Invalid Arguments!`
+//     }
+// }
+
+// let productResult = product(10, 20)
+// console.log(productResult);
+
+// function greet(name) {
+//     console.log(`Good evening ${name}`);
+// }
+
+// function add(a, b) {
+//     return (a + b)
+// }
+
+// // Calling the function
+// greet('Vaishnavi')
+
+// const result = add(10, 20)
+// console.log(`The result of 10 + 20 is ${result}`);
+
+
+// // ES6 Arrow Function ---> accessModifier ---> 
+// const greet = (name) => {
+//     console.log(`Good evening ${name}`);
+// }
+
+// let functionName/variableName = (parameter(s)) => { ...bodyOfTheFUnction }
+// let newProduct = (a, b) => {
+//     if(typeof a == 'number' && typeof b == 'number') {
+//         return a * b
+//     } else {
+//         return `Invalid Arguments!`
+//     }
+// }
+
+// let newProductResult = newProduct(100, 200)
+// console.log(newProductResult)
